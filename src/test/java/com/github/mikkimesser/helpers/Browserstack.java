fe2203887693351a00e051e361e9343e90e8eb13
Mike Bulanov <mikki.messer@gmail.com>
2022-06-29 22:44:44 +0300

    first commit: added framework for implementing automated mobile tests on browserstack

---
+++ src/test/java/com/github/mikkimesser/helpers/Browserstack.java
@@ -0,0 +1,24 @@
+package com.github.mikkimesser.helpers;
+
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
+
+public class Browserstack {
+
+    public static String videoUrl(String sessionId) {
+        String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+        //TODO: add owner-stored credentials
+        return given()
+                .auth().basic("mylogin", "mypasswd")
+                .log().all()
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .path("automation_session.video_url");
+    }
+}
+
